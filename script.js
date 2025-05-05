d7e032c3ee2b341d38e5ad98a4bfd32b17a04753
joaovidalsenai <joao.vidal@ba.estudante.senai.br>
2025-05-05 16:22:17 -0300

    Facilimo

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function validarInscricao(event) {
     event.preventDefault();
     let nomeAluno = document.getElementById("input-nome").value.split(" ")[0];
-    alert(`Inscrição realizada com sucesso! Boa sorte no campeonato ${nomeAluno}!`);
+    ///document.getElementById("cadastro-finalizado").innerHTML = `Inscrição realizada com sucesso! Boa sorte no campeonato ${nomeAluno}!`;
+    alert(`Inscrição realizada com sucesso! Boa sorte no campeonato ${nomeAluno}!`)
 }
 
 function mudarSecao(secao) {
@@ -15,10 +16,32 @@ function mudarSecao(secao) {
             document.getElementById("card-info").style.display = 'block'
             break;
         case 3:
-            document.getElementById("form-inscricao").style.display = 'block'
+            document.getElementById("form-inscricao").style.display = 'flex'
             break;
     
         default:
+            document.getElementById("card-info").style.display = 'block'
             break;
     }
 }
+
+const dataCampeonato = new Date(2025, 4, 15, 15, 30, 0);
+    
+    function atualizarContador() {
+        const agora = new Date();
+        const diferenca = dataCampeonato - agora;
+
+        if (diferenca <= 0) {
+            document.getElementById("contador").innerText = "O campeonato já começou!";
+            return;
+        }
+
+        const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+        const horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
+        const segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
+        document.getElementById("contador").innerText = `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos para o campeonato começar!`;
+    }
+
+atualizarContador();
+setInterval(atualizarContador, 1000);
